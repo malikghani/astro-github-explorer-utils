f99e4ffc84aeb42d4ff43aa7366bbba5e01f14f5
Malik Abdul Ghani <hi@malikghani.com>
2025-10-19 16:42:44 +0700

    Updated package.swift, set the constants properties to public

--- Sources/GithubExplorerUtils/Constants.swift
+++ Sources/GithubExplorerUtils/Constants.swift
@@ -10,23 +10,23 @@ import Foundation
 /// A container for global constants used across the app.
 public enum Constants {
     /// The base URL for the GitHub API.
-    static let baseURL = URL(string: "https://api.github.com")!
+    public static let baseURL = URL(string: "https://api.github.com")!
     
     /// The debounce duration (in nanoseconds) used to reduce the frequency of requests and help prevent hitting GitHub’s rate limits.
-    static let debounceDuration: UInt64 = 1_500_000_000
+    public static let debounceDuration: UInt64 = 1_500_000_000
     
     /// The number of search results to fetch per page when requesting data from the GitHub API.
-    static let searchPageCount = 15
+    public static let searchPageCount = 15
     
     /// The distance from the end of the currently loaded users list that triggers pagination.
-    static let paginationTreshold = 5
+    public static let paginationTreshold = 5
     
     /// The maximum number of objects that can be stored in the cache.
-    static let cacheCountLimit = 250
+    public static let cacheCountLimit = 250
 
     /// The total cost limit of the cache in bytes.
-    static let cacheTotalCostLimit = 100 * 1024 * 1024 // 100 MB
+    public static let cacheTotalCostLimit = 100 * 1024 * 1024 // 100 MB
     
     /// The duration of the fade animation used when transitioning between image loading states.
-    static let avatarFadeDuration: TimeInterval = 0.25
+    public static let avatarFadeDuration: TimeInterval = 0.25
 }
